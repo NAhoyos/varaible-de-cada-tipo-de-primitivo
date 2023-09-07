f709cca32635e77bbc70658f56c0c66d06662f0a
Nelson Hoyos <nahoyos30@gmail.com>
2023-09-07 11:25:21 -0500

    ejercicios de concatenar

---
+++ concatenacion.py
@@ -0,0 +1,21 @@
+# comentarios
+
+# los numeros enteros no tienen limite y puede expandirse dependiendo de la cantidad de bits de la maquina.
+# los numeros flotentes o Los float a diferencia de los int tienen unos valores mínimo y máximos que pueden representar. La mínima precisión es 2.2250738585072014e-308 y la máxima 1.7976931348623157e+308
+
+
+num_entero = 4
+num_real = 23, 567
+saludo = "hola mundo"
+resultado = True
+
+print(num_entero)
+print(num_real)
+print(saludo)
+print(resultado)
+concatenar = (str(num_entero), str(num_real), str(saludo), str(resultado))
+
+
+print(concatenar)
+
+num_entero = input("numero enteros")
